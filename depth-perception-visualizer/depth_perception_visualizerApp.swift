e37dda87c8aaef0fb35f6122bebec0a682887094
dngg <mingjungdanielsch@gmail.com>
2025-07-16 16:11:53 +1000

    Initial Commit

---
+++ depth-perception-visualizer/depth_perception_visualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  depth_perception_visualizerApp.swift
+//  depth-perception-visualizer
+//
+//  Created by Daniel Ng on 16/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct depth_perception_visualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ depth-perception-visualizerTests/depth_perception_visualizerTests.swift
@@ -0,0 +1,17 @@
+//
+//  depth_perception_visualizerTests.swift
+//  depth-perception-visualizerTests
+//
+//  Created by Daniel Ng on 16/7/2025.
+//
+
+import Testing
+@testable import depth_perception_visualizer
+
+struct depth_perception_visualizerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
